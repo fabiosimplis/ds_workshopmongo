be66feae278d6d4efe837f0e203f9778bf9975bd
fabiosimplis <fabio.simplis@gmail.com>
2024-10-24 19:10:53 -0300

    ADD GET user by Id

--- workshopmongo/src/main/java/br/com/fjunior/workshopmongo/controllers/UserController.java
+++ workshopmongo/src/main/java/br/com/fjunior/workshopmongo/controllers/UserController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fjunior.workshopmongo.models.dto.UserDTO;
 import br.com.fjunior.workshopmongo.services.UserService;
+
 
 
 @RestController
@@ -24,4 +26,12 @@ public class UserController {
         List<UserDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        var body = service.findById(id);
+        return ResponseEntity.ok().body(body);
+    }
+    
+    
 }
--- workshopmongo/src/main/java/br/com/fjunior/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/br/com/fjunior/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package br.com.fjunior.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fjunior.workshopmongo.models.dto.UserDTO;
+import br.com.fjunior.workshopmongo.models.entities.User;
 import br.com.fjunior.workshopmongo.repositories.UserRepository;
+import br.com.fjunior.workshopmongo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -16,8 +19,12 @@ public class UserService {
 
     public List<UserDTO> findAll() {
         var users = repository.findAll();
-        System.out.println("SERVICE");
-        System.out.println(users);
         return users.stream().map(UserDTO::new).toList();
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> result = repository.findById(id);
+        User user = result.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+        return new UserDTO(user);
     }
 }
